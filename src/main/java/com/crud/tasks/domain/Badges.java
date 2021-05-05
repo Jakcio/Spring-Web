5e39bebf9c5a30e24c3e16931d06838ec0f2994b
Jakub Ciosek <ciosekjakub@wp.pl>
2021-05-05 15:08:49 +0200

    "22.3 task"

---
+++ src/main/java/com/crud/tasks/domain/Badges.java
@@ -0,0 +1,13 @@
+package com.crud.tasks.domain;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Badges {
+
+    @JsonProperty("votes")
+    private int votes;
+
+    @JsonProperty("attachmentsByType")
+    private AttachmentsByType attachments;
+
+}
