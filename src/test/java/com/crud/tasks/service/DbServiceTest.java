648e6db61346bf5bdb7fdfb5ee65bff7847a6606
Jakub Ciosek <ciosekjakub@wp.pl>
2021-05-26 21:20:11 +0200

    29.2 fix

--- src/test/java/com/crud/tasks/service/DbServiceTest.java
+++ src/test/java/com/crud/tasks/service/DbServiceTest.java
@@ -1,13 +1,16 @@
 package com.crud.tasks.service;
 
 import com.crud.tasks.domain.Task;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.util.AssertionErrors.assertFalse;
 
 @SpringBootTest
 public class DbServiceTest {
@@ -52,4 +55,32 @@ public class DbServiceTest {
         //CleanUp
         dbService.deleteTaskByID(id1);
     }
+
+    @Test
+    void shouldSaveTask() {
+        //Given
+        Task task1 = new Task(1L, "name1", "desc1");
+        //When
+        long id1 = dbService.saveTask(task1).getId();
+        //Then
+        assertNotEquals(0, id1);
+        assertEquals(task1.getTitle(), dbService.getTask(id1).get().getTitle());
+        assertEquals(task1.getContent(), dbService.getTask(id1).get().getContent());
+        //CleanUp
+        dbService.deleteTaskByID(id1);
+    }
+
+    @Test
+    void shouldDeleteTask() {
+        //Given
+        Task task1 = new Task(1L, "name1", "desc1");
+        long id1 = dbService.saveTask(task1).getId();
+        //When
+        dbService.deleteTaskByID(id1);
+        //Then
+        Assertions.assertFalse(dbService.getTask(id1).isPresent());
+    }
+
+
+
 }
